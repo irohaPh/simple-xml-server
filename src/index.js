5b2cad06ebc98455b786f07072efc26125475a2f
irohaPh <photo.mana.shr@gmail.com>
2019-07-04 21:19:10 +0900

    設定ファイルに従ってレスポンスを切り替えられるように修正

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const server = http.createServer();
 const host = '127.0.0.1';
 const port = process.env.NODE_PORT || process.argv[2] || 3000;
 
+const settings = require('../conf/response.json');
+
 server.on('request', function(req, res) {
   const { method, url } = req;
   const { path, pathname, query } = urlParser.parse(url);
@@ -13,19 +15,34 @@ server.on('request', function(req, res) {
     `request received. method: [${method}] path: [${path}] pathname: [${pathname}] query: [${query}]`
   );
 
-  let xmlFile = 'response/';
-  switch (true) {
-    case /^\/sample/.test(pathname):
-      xmlFile += 'sample.xml';
-      break;
+  /* リクエストされたパスにマッチする設定を取得(見つからなければデフォルト設定を取得) */
+  const matchSetting =
+    settings
+      .filter(s => s.path !== undefined)
+      .filter(s => {
+        let reg = new RegExp(
+          (s.match === 'backward' ? '' : '^/') +
+            s.path +
+            (s.match === 'forward' ? '' : '$')
+        );
+        return pathname.match(reg);
+      })
+      .shift() || settings.filter(s => s.default).shift();
 
-    default:
-      xmlFile += 'default.xml';
-      break;
+  /* 設定に応じたレスポンスの生成 */
+  if (matchSetting) {
+    try {
+      let xmlFile = `response/${matchSetting.response}`;
+      fs.statSync(xmlFile); // ファイルが存在しない場合は例外を投げる
+
+      res.writeHead(200, { 'Content-Type': 'application/xml; charset=UTF-8' });
+      res.write(fs.readFileSync(xmlFile, { encoding: 'utf-8' }));
+    } catch (err) {
+      res.writeHead(500, { 'Content-Type': 'application/xml; charset=UTF-8' });
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/xml; charset=UTF-8' });
   }
-
-  res.writeHead(200, { 'Content-Type': 'application/xml; charset=UTF-8' });
-  res.write(fs.readFileSync(xmlFile, { encoding: 'utf-8' }));
 
   res.end();
 });
